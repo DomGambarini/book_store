d62e43728a3f84340755f70bf6bb28ddcbb16bdf
Dominic Gambarini <domgambarini@gmail.com>
2024-03-24 15:46:50 +0000

    Added add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -14,7 +14,7 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
-
+    redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
@@ -23,4 +23,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
