9fb06ed50f69f49af4335c48603e7bff65fb6ecc
Dominic Gambarini <domgambarini@gmail.com>
2024-03-29 12:02:32 +0000

    Added stripe to views and set up elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OzdpZLAOnNQgz8Rh2ODSxdfJJ5ZjBgW6hQHHTUx9Kz4dmMIeU49JvsX8OWCVMIRvuWUIzmqAqG7wVORF3vTjw84008cKaGy0S',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
