a2ade567b5e78b58b174445c1e1a64c318817f82
Dominic Gambarini <domgambarini@gmail.com>
2024-04-04 21:18:31 +0100

    Trying to connect heroku to site

--- indie_book_emporium/settings.py
+++ indie_book_emporium/settings.py
@@ -18,6 +18,7 @@ from pathlib import Path
 # Load environment variables from the .env file
 load_dotenv()
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
