410b137e3384faf81bcf32eb91e7310770b9b3f5
Dominic Gambarini <domgambarini@gmail.com>
2024-04-04 15:11:06 +0100

    removed secert key and set debug

--- indie_book_emporium/settings.py
+++ indie_book_emporium/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*f4^4)6gw58o!in5aovvg_zg)2zmrrgq*&d57zqp9y1x8@&-j2'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['indie-book-emporium.herokuapp.com', 'localhost']
 
