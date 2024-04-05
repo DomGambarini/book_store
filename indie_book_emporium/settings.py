cea7e08f8c2a5e7ea2c08a1112cb547c36a7bc8b
Dominic Gambarini <domgambarini@gmail.com>
2024-04-05 13:38:34 +0100

    Added [::1] to allowed hosts

--- indie_book_emporium/settings.py
+++ indie_book_emporium/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['https://indie-book-emporium-b10db93763d5.herokuapp.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['https://indie-book-emporium-b10db93763d5.herokuapp.com/', '127.0.0.1', 'localhost', '[::1]']
 
 
 # Application definition
