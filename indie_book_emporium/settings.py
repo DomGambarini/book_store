2cb6e64400cd55660e7814fb4c14db4b762bb94f
Dominic Gambarini <domgambarini@gmail.com>
2024-04-06 14:43:27 +0000

    Typo in settings which has been fixed

--- indie_book_emporium/settings.py
+++ indie_book_emporium/settings.py
@@ -190,8 +190,8 @@ if 'USE_AWS' in os.environ:
 
     AWS_STORAGE_BUCKET_NAME = 'indie-book-emporium'
     AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = os.getenv.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv.get('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
