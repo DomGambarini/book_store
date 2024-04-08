c3ba371392cec9187a095ed260efe6236bdcc000
Dominic Gambarini <domgambarini@gmail.com>
2024-04-08 20:14:34 +0000

    Fixed indention error in team view

--- team/views.py
+++ team/views.py
@@ -7,10 +7,9 @@ from .models import Team
 def team(request):
     """ A view to return the team page """
 
-    team_member = Team.objects.all(
-        context = {
-            'team_member': team_member
-        }
-    )
+    team_member = Team.objects.all()
+    context = {
+        'team_member': team_member
+    }
     return render(request, 'team/team.html', context)
 
