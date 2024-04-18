181be4af46c14c8795186ed51bebf0fe0ef45159
Dominic Gambarini <domgambarini@gmail.com>
2024-04-18 09:34:47 +0000

    Activating real emails to be sent when making a purchase

--- indie_book_emporium/settings.py
+++ indie_book_emporium/settings.py
@@ -112,8 +112,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  # allow users to log in using either their username or email address
 ACCOUNT_EMAIL_REQUIRED = True  # require email address when signing up
 ACCOUNT_EMAIL_VERIFICATION = 'none'  # require email verification before the account becomes active
@@ -220,4 +218,15 @@ STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 STRIPE_CURRENCY = 'usd'
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET")
-DEFAULT_FROM_EMAIL = 'indiebookemporiumexample@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'indiebookemporiumexample@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM_EMAIL')
