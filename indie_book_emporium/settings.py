98f3e4629d6b34890348a75ffdb6b3a2143f50a5
Dominic Gambarini <domgambarini@gmail.com>
2024-04-18 09:47:58 +0000

    Debugged typo in settings for email setup

--- indie_book_emporium/settings.py
+++ indie_book_emporium/settings.py
@@ -229,4 +229,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM_EMAIL')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
