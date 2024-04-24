94205caa11d1db8837f89bf16e19918e542f798e
Dominic Gambarini <domgambarini@gmail.com>
2024-04-24 11:22:07 +0000

    profile picture in team modeld a required field

--- team/models.py
+++ team/models.py
@@ -7,7 +7,8 @@ class Team(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     position = models.CharField(max_length=50, null=False, blank=False)
     bio = models.TextField(max_length=1000, null=False, blank=False)
-    profile_picture = models.ImageField(null=True, blank=True)
+    profile_picture = models.ImageField(
+        default='/media/noimage.png', null=False, blank=False)
 
     def __str__(self):
         return self.name
